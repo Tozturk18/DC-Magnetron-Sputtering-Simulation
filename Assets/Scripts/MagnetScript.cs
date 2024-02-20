14948d84b1d77553dcf75bf8c3ebd22b2b636c56
tozturk18 <tunaozturk2001@hotmail.com>
2024-02-20 14:46:44 -0500

    Adding Magnetic Dipole Moment values

--- Assets/Scripts/MagnetScript.cs
+++ Assets/Scripts/MagnetScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,31 @@ using UnityEngine;
 public class MagnetScript : MonoBehaviour {
 
     public int magnetType = 0;
-    public float remanencefield = 0.0f;
+    public float remanence_field = 0.0f;
+    public float dipole_moment;
+    private float permiability = 4*Math.PI()*pow(10,-7) (Tesla * meter / Ampere);
 
     // Start is called before the first frame update
-    /*void Start() {
-        
+    void Start() {
+        dipole_moment = remanence_field * VolumeOfMesh(gameobject.mesh) / permiability;
     }
 
+    /*
     // Update is called once per frame
     void Update() {
         
     }*/
+
+    float VolumeOfMesh(Mesh mesh) {
+        float volume = 0;
+        Vector3[] vertices = mesh.vertices;
+        int[] triangles = mesh.triangles;
+        for (int i = 0; i < mesh.triangles.Length; i += 3) {
+            Vector3 p1 = vertices[triangles[i + 0]];
+            Vector3 p2 = vertices[triangles[i + 1]];
+            Vector3 p3 = vertices[triangles[i + 2]];
+            volume += SignedVolumeOfTriangle(p1, p2, p3);
+        }
+        return Mathf.Abs(volume);
+    }
 }
