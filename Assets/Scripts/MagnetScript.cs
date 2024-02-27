ee44d11e2641214021a41d2f1295d0b027c81dee
Tozturk18 <tunaozturk2001@hotmail.com>
2024-02-26 19:19:36 -0500

    Added dipole_moment calculation

--- Assets/Scripts/MagnetScript.cs
+++ Assets/Scripts/MagnetScript.cs
@@ -1,3 +1,4 @@
+using System.Xml.Schema;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,11 +9,15 @@ public class MagnetScript : MonoBehaviour {
     public int magnetType = 0;
     public float remanence_field = 0.0f;
     public float dipole_moment;
-    private float permiability = 4*Math.PI()*pow(10,-7) (Tesla * meter / Ampere);
+    private float permeability = 4*Mathf.PI * Mathf.Pow(10,-7); // (Tesla * meter / Ampere);
 
     // Start is called before the first frame update
     void Start() {
-        dipole_moment = remanence_field * VolumeOfMesh(gameobject.mesh) / permiability;
+        float volume = Mathf.PI * transform.localScale.x * transform.localScale.x * transform.localScale.y;
+
+        dipole_moment = remanence_field * volume / this.permeability;
+        
+        Debug.Log(dipole_moment);
     }
 
     /*
@@ -20,17 +25,4 @@ public class MagnetScript : MonoBehaviour {
     void Update() {
         
     }*/
-
-    float VolumeOfMesh(Mesh mesh) {
-        float volume = 0;
-        Vector3[] vertices = mesh.vertices;
-        int[] triangles = mesh.triangles;
-        for (int i = 0; i < mesh.triangles.Length; i += 3) {
-            Vector3 p1 = vertices[triangles[i + 0]];
-            Vector3 p2 = vertices[triangles[i + 1]];
-            Vector3 p3 = vertices[triangles[i + 2]];
-            volume += SignedVolumeOfTriangle(p1, p2, p3);
-        }
-        return Mathf.Abs(volume);
-    }
 }
